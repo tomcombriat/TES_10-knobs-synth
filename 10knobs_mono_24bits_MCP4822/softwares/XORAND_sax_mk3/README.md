18be9ca4093d3e9f413c0ea7ad7fe0f5fd0638b3
tomcombriat <tomcombriat@live.fr>
2021-05-14 20:58:09 +0200

    Added non-aliased version of XORAND

---
+++ 10knobs_mono_24bits_MCP4822/softwares/XORAND_sax_mk3/README.md
@@ -0,0 +1,10 @@
+## TES - XORAND sax mk2
+T. Combriat
+
+
+A version of [XORAND synth](https://github.com/tomcombriat/8_knobs_synth/tree/master/XORAND_poly) to be used with an EWI or the [TESaxophone](https://t-e-s.frama.site/en/tesaxophone).
+Differences include:
+* Polyphony: 4
+* Chords on separate MIDI channel
+* Breath controlled volume and filter
+
---
+++ 10knobs_mono_24bits_MCP4822/softwares/XORAND_sax_mk3/midi_handles.ino
@@ -0,0 +1,208 @@
+#include "midi_handles.h"
+#include "oscil_declaration.h"
+
+
+
+/*********************
+       NOTE ON
+ ********************/
+void HandleNoteOn(byte channel, byte note, byte velocity)
+{
+  byte min_rank = 255;
+  int empty_arg = -1;
+  for (byte i = 0; i < POLYPHONY; i++)  //take a non playing oscil
+  {
+    if (!envelope[i].playing() && oscil_rank[i] < min_rank)
+    {
+      empty_arg = i;
+      min_rank = oscil_rank[i];
+    }
+  }
+
+  if (empty_arg == -1)  //kill a oscil in release phase
+  {
+    min_rank = 255;
+    for (byte i = 0; i < POLYPHONY; i++)
+    {
+      if (oscil_state[i] == 0 && oscil_rank[i] < min_rank)
+      {
+        empty_arg = i;
+        min_rank = oscil_rank[i];
+      }
+    }
+  }
+
+
+  if (empty_arg == -1)  // no empty oscil, kill one in sustain mode! (the oldest)
+  {
+    min_rank = 255;
+    byte min_rank_arg = 0;
+    for (byte i = 0; i < POLYPHONY; i++)
+    {
+      if (oscil_rank[i] < min_rank)
+      {
+        min_rank = oscil_rank[i];
+        min_rank_arg  = i;
+      }
+    }
+    empty_arg = min_rank_arg;
+  }
+
+
+  notes[empty_arg] = note;
+  set_freq(empty_arg);
+
+  if (channel == 2)
+  {
+    envelope[empty_arg].setAttackTime(1);
+    envelope[empty_arg].noteOn();
+  }
+
+  else if (channel == 3 && chord_attack > 5 )
+  {
+    envelope[empty_arg].setAttackTime(chord_attack);
+    envelope[empty_arg].noteOn(true);
+  }
+  else 
+  {
+    envelope[empty_arg].setAttackTime(1);
+    envelope[empty_arg].noteOn();
+  }
+
+  oscil_state[empty_arg] = 1;
+
+
+
+
+  byte max_rank = 0;
+  for (byte i = 0; i < POLYPHONY; i++)
+  {
+    if (oscil_rank[i] > max_rank) max_rank = oscil_rank[i];
+  }
+  oscil_rank[empty_arg] = max_rank + 1;
+
+
+  // shift all oscill
+  min_rank = 255;
+  for (byte i = 0; i < POLYPHONY; i++)
+  {
+    if (oscil_rank[i] < min_rank)
+    {
+      empty_arg = i;
+      min_rank = oscil_rank[i];
+    }
+  }
+  if (min_rank != 0) for (byte i = 0; i < POLYPHONY; i++) oscil_rank[i] -= min_rank;
+
+  volume = velocity;
+
+}
+
+
+/**********************
+      NOTE OFF
+ *********************/
+
+
+void HandleNoteOff(byte channel, byte note, byte velocity)
+{
+  byte to_kill = 255;
+  byte min_rank = 255;
+
+  for (byte i = 0; i < POLYPHONY; i++)
+  {
+    if (note == notes[i] && oscil_state[i] == 1 && oscil_rank[i] < min_rank  )
+    {
+      min_rank = oscil_rank[i];
+      to_kill = i;
+    }
+  }
+
+  if (to_kill != 255)
+  {
+    if (channel == 2)  envelope[to_kill].setReleaseTime(1);
+    else if (channel == 3) envelope[to_kill].setReleaseTime(chord_release);
+    envelope[to_kill].noteOff();
+    oscil_state[to_kill] = 0;
+  }
+
+  for (byte i = 0; i < POLYPHONY; i++)
+  {
+    if (oscil_rank[i] > oscil_rank[to_kill]) oscil_rank[i] -= 1;
+  }
+}
+
+
+
+/*************************
+  CONTROL CHANGE
+************************/
+void HandleControlChange(byte channel, byte control, byte val)
+{
+  switch (control)
+  {
+    case 64:    // sustain
+      if (val == 0)
+      {
+        sustain = false;
+        for (byte i = 0; i < POLYPHONY; i++)
+        {
+          if (oscil_state[i] == 2)
+          {
+            envelope[i].noteOff();
+            oscil_state[i] = 0;
+          }
+        }
+      }
+      else sustain = true;
+      break;
+
+
+    case 1:  //modulation
+      for (byte i = 0; i < POLYPHONY; i++) LFO[i].setFreq_Q16n16((Q16n16) (val << 13 ));
+      if (val == 0 && mod) mod = false;
+      else if (val != 0 && !mod) mod = true;
+      break;
+
+    case 74: //volume
+      volume = val;
+      /*if (val == 0) {
+        for (byte i = 0; i < POLYPHONY; i++) osc_is_on[i] = false;
+        }*/
+      break;
+
+    case 71: //cutoff
+      midi_cutoff = val;
+      break;
+
+    case 5: // pitchbend_amp
+      pitchbend_amp = val;
+      break;
+
+  }
+}
+
+
+
+/**********************
+      PITCHBEND
+ *    ***************/
+void HandlePitchBend(byte channel, int bend)
+{
+  pitchbend = bend;
+  for (byte i = 0; i < POLYPHONY; i++)
+  {
+    if (envelope[i].playing()) set_freq(i, false);
+  }
+}
+
+/*********************
+   AFTERTOUCH
+ * *******************/
+
+void HandleAfterTouchChannel(byte channel, byte after)
+{
+  aftertouch = after;
+}
+
+
