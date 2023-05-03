1b2c5cc34e10cebc80c5b18a82797b5b24b49f8a
tomcombriat <tomcombriat@live.fr>
2023-05-03 21:07:31 +0200

    Fixed static allocation of oscillators

--- 10knobs_mono_16bits_PT8211/softwares/DIGITAL_SENAP_sax/DIGITAL_SENAP_sax.ino
+++ 10knobs_mono_16bits_PT8211/softwares/DIGITAL_SENAP_sax/DIGITAL_SENAP_sax.ino
@@ -13,21 +13,21 @@
 */
 
 
-#define RINGING_CHORDS // chords do not follow breath when in the decaying phase if the breath is higher than their noteOff velocity
-#define DITHERING   // dithering before shifting down to 16 bits
+//#define RINGING_CHORDS // chords do not follow breath when in the decaying phase if the breath is higher than their noteOff velocity
+#define DITHERING  // dithering before shifting down to 16 bits
 #define LEGACYSTM  // this code is normally compiled with the "original" STM32duino. Uncomment this line to use the ST supported core.
 #include <MIDI.h>
 #include <MozziGuts.h>
 #include <Oscil.h>
-#include<MetaOscil.h>
-#include<SPI.h>
+#include <MetaOscil.h>
+#include <SPI.h>
 
 #include <mozzi_rand.h>
 #include <mozzi_midi.h>
 #include <mozzi_fixmath.h>
 #include <Smooth.h>
 #include <ADSR.h>
-#include <LowPassFilter.h>
+#include <ResonantFilter.h>
 #include <AudioDelayFeedback.h>
 #include <Portamento.h>
 #include "midi_handles.h"
@@ -37,38 +37,48 @@
 #include <tables/cos2048_int8.h>
 
 
-#define CONTROL_RATE 2048 // Hz, powers of 2 are most reliable
-//#define CONTROL_RATE 512
+#define CONTROL_RATE 4096  // Hz, powers of 2 are most reliable
+//#define CONTROL_RATE 2048
 
 
 #define LED PA8
 
-#define POLYPHONY 12
+//#define POLYPHONY 12
+#define POLYPHONY 10
 
 
-Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aCarrier[POLYPHONY] = {Oscil<COS2048_NUM_CELLS, AUDIO_RATE> (COS2048_DATA)};
-Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aMod[POLYPHONY] = {Oscil<COS2048_NUM_CELLS, AUDIO_RATE> (COS2048_DATA)};
+Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aCarrier[POLYPHONY] = { Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                             Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                             Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                             Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                             Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA) };
+
+Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aMod[POLYPHONY] = { Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                         Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                         Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                         Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA),
+                                                         Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA), Oscil<COS2048_NUM_CELLS, AUDIO_RATE>(COS2048_DATA) };
 
 
-ADSR <AUDIO_RATE, AUDIO_RATE, unsigned long> envelope[POLYPHONY];
+ADSR<AUDIO_RATE, AUDIO_RATE, unsigned long> envelope[POLYPHONY];
 
 LowPassFilter16 lpf;
-Smooth <unsigned int> cutoff_smooth(0.995f);  // 0.999 -> 15ms  // a bit sluggish?// 0.995 -> 7ms
-Smooth <unsigned int> rm_smooth(0.995f);  // 0.999 -> 15ms  // a bit sluggish?// 0.995 -> 7ms
-Smooth <int> breath_smooth(0.98f);  // if updated at AUDIO_RATE:
+Smooth<unsigned int> cutoff_smooth(0.995f);  // 0.999 -> 15ms  // a bit sluggish?// 0.995 -> 7ms
+Smooth<unsigned int> rm_smooth(0.995f);      // 0.999 -> 15ms  // a bit sluggish?// 0.995 -> 7ms
+Smooth<int> breath_smooth(0.98f);            // if updated at AUDIO_RATE:
 // 0.99 -> 15ms maximal raise time
 // 0.98 -> 8ms
 // 0.9999 -> 1.3s (!!!!)
 
-Smooth <Q16n16> deviation_smooth[POLYPHONY] = {Smooth <Q16n16>(0.001f)};
+Smooth<Q16n16> deviation_smooth[POLYPHONY] = { Smooth<Q16n16>(0.001f) };
 
-byte notes[POLYPHONY] = {100};
+byte notes[POLYPHONY] = { 100 };
 
-int  pitchbend = 0, pitchbend_amp = 2, resonance = 0, breath_sens = 0, volume = 0, breath_on_cutoff = 0, breath_on_rm = 0, prev_resonance = 0, toggle = 0;
+int pitchbend = 0, pitchbend_amp = 2, resonance = 0, breath_sens = 0, volume = 0, breath_on_cutoff = 0, breath_on_rm = 0, prev_resonance = 0, toggle = 0;
 byte oscil_state[POLYPHONY], oscil_rank[POLYPHONY], runner = 0, prev_MSB_volume = 0;
 bool sustain = false;
-bool osc_is_on[POLYPHONY] = {false};
-int breath_at_note_off[POLYPHONY] = {0}, breath_next = 0;
+bool osc_is_on[POLYPHONY] = { false };
+int breath_at_note_off[POLYPHONY] = { 0 }, breath_next = 0;
 unsigned int chord_attack = 1, chord_release = 1, cutoff = 0, prev_cutoff = 0, midi_cutoff = 0;
 uint32_t deviation_rm;
 byte breath_on_vol_LUT[256];
@@ -95,32 +105,29 @@ Q16n16 deviation_rm_pot;
 #ifdef LEGACYSTM
 SPIClass mySPI(2);
 #else
-SPIClass mySPI(PB15,PB14,PB13);
+SPIClass mySPI(PB15, PB14, PB13);
 #endif
 #define WS_pin PB8
 
 
 #ifndef LEGACYSTM
-HardwareSerial Serial3(PB11,PB10);
+HardwareSerial Serial3(PB11, PB10);
 #endif
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial3, MIDI);
 
 
-void set_freq(byte i, bool reset_phase = false)
-{
-  //Serial.println("Entering set freq");
+void set_freq(byte i, bool reset_phase = false) {
   osc_is_on[i] = true;
-  Q16n16 current_note = Q8n0_to_Q16n16(notes[i]) + (pitchbend << 3) * pitchbend_amp; // maybe put the shift here instead ?
+  Q16n16 current_note = Q8n0_to_Q16n16(notes[i]) + (pitchbend << 3) * pitchbend_amp;  // maybe put the shift here instead ?
   Q16n16 freq = Q16n16_mtof(current_note);
-  freq = freq >> 1; // For stable FM, next need to be called, leading to twice the freq as a result
+  freq = freq >> 1;  // For stable FM, next need to be called, leading to twice the freq as a result
 
   carrier_freq[i] = freq;
 
   compute_fm_param(i, reset_phase);
 }
 
-inline void compute_fm_param(byte i, bool reset_phase)
-{
+inline void compute_fm_param(byte i, bool reset_phase) {
   /*  mod_freq[i] = ((carrier_freq[i] >> 8) * mod_to_carrier_ratio);
 
     aMod[i].setFreq_Q16n16(mod_freq[i]);*/
@@ -136,8 +143,7 @@ inline void compute_fm_param(byte i, bool reset_phase)
   aCarrier[i].setFreq_Q16n16(carrier_freq[i]);
   aMod[i].setFreq_Q24n8(mod_freq[i]);
   //aMod[i].setPhaseFractional(aCarrier[i].getPhaseFractional());
-  if (reset_phase)
-  {
+  if (reset_phase) {
     aCarrier[i].setPhase(0);
     aMod[i].setPhase(0);
   }
@@ -160,16 +166,13 @@ inline void compute_fm_param(byte i, bool reset_phase)
 
 
 
-int three_values_knob(int val, int i)
-{
-  switch (i)
-  {
+int three_values_knob(int val, int i) {
+  switch (i) {
     case 0:
       {
         if (val > 512) {
           return 0;
-        }
-        else return (512 - val);
+        } else return (512 - val);
       }
     case 1:
       {
@@ -178,7 +181,7 @@ int three_values_knob(int val, int i)
       }
     case 2:
       {
-        if (val < 512 ) return 0;
+        if (val < 512) return 0;
         else return val - 512;
       }
   }
@@ -188,11 +191,10 @@ int three_values_knob(int val, int i)
 
 
 void setup() {
-  Serial1.begin(115200);
   pinMode(LED, OUTPUT);
   mySPI.begin();
   delay(100);
-  mySPI.beginTransaction(SPISettings(200000000, MSBFIRST, SPI_MODE0)); //MSB first, according to the DAC spec
+  mySPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));  //MSB first, according to the DAC spec
 
   pinMode(WS_pin, OUTPUT);
 
@@ -206,15 +208,12 @@ void setup() {
   pinMode(PA6, INPUT);
   pinMode(PA7, INPUT);
 
-
-  for (int i = 0; i < 256; i++)
-  {
-    if (i != 0)  breath_on_vol_LUT[i] = log((float) i) / log(255.) * 255;
+  for (int i = 0; i < 256; i++) {
+    if (i != 0) breath_on_vol_LUT[i] = log((float)i) / log(255.) * 255;
     else breath_on_vol_LUT[i] = 0;
   }
 
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
+  for (byte i = 0; i < POLYPHONY; i++) {
     envelope[i].setADLevels(128, 128);
     envelope[i].setTimes(1, 1, 6500000, 10);
   }
@@ -239,7 +238,7 @@ void setup() {
 
 
   MIDI.begin(MIDI_CHANNEL_OMNI);
-  MIDI.turnThruOff (); // hardwired
+  MIDI.turnThruOff();  // hardwired
   digitalWrite(LED, HIGH);
   delay(100);
   digitalWrite(LED, LOW);
@@ -253,8 +252,6 @@ void setup() {
 
   startMozzi(CONTROL_RATE);
   digitalWrite(LED, LOW);
-
-
 }
 
 
@@ -270,7 +267,7 @@ void loop() {
 
 
 
-void audioOutput(const AudioOutput f) // f is a structure containing both channels
+void audioOutput(const AudioOutput f)  // f is a structure containing both channels
 {
   digitalWrite(WS_pin, LOW);  //select Right channel
   mySPI.transfer16(f.l());
@@ -281,20 +278,14 @@ void audioOutput(const AudioOutput f) // f is a structure containing both channe
 
 
 void updateControl() {
-//  while (MIDI.read());
-while (MIDI.read())
-{
-      digitalWrite(LED, HIGH);
-}
-Serial3.println("c");
-  
-  if ((volume) == 0)
-  {
-    for (byte i = 0; i < POLYPHONY; i++)
-    {
+  while (MIDI.read())
+    ;
+
+  if ((volume) == 0) {
+    for (byte i = 0; i < POLYPHONY; i++) {
       envelope[i].noteOff();
-      osc_is_on[i] = false; // so that chord do not fade out during next note afer a short pause
-      oscil_state[i] = 0;   // everybody reset
+      osc_is_on[i] = false;  // so that chord do not fade out during next note afer a short pause
+      oscil_state[i] = 0;    // everybody reset
       volume = 0;
     }
   }
@@ -302,39 +293,30 @@ Serial3.println("c");
 
   toggle++;
 
-  switch (toggle)
-  {
+  switch (toggle) {
     case 1:
       //mod_to_carrier_ratio = mozziAnalogRead(PA6) >> 2;  //10bits: 2integers, 8 fractionnal (up to 4)
       mod_to_carrier_ratio = dual_type_pot(mozziAnalogRead(PA6));
-      if (mod_to_carrier_ratio != mod_to_carrier_ratio_old)
-      {
+      if (mod_to_carrier_ratio != mod_to_carrier_ratio_old) {
         mod_to_carrier_ratio_old = mod_to_carrier_ratio;
-        for (byte i = 0; i < POLYPHONY; i++)  compute_fm_param(i, false);
+        for (byte i = 0; i < POLYPHONY; i++) compute_fm_param(i, false);
       }
       break;
     case 2:
-      deviation_pot = mozziAnalogRead(PB0) << 10 ;
-      for (byte i = 0; i < POLYPHONY; i++)
-      {
+      deviation_pot = mozziAnalogRead(PB0) << 10;
+      for (byte i = 0; i < POLYPHONY; i++) {
         if (osc_is_on[i]) deviation[i] = min(maxDeviation[Q8n8_to_Q8n0(mod_to_carrier_ratio)][Q16n16_to_Q16n0(Q8n0_to_Q16n16(notes[i]) + (pitchbend << 3) * pitchbend_amp)], deviation_pot);
-        /*   Serial.print(maxDeviation[Q8n8_to_Q8n0(mod_to_carrier_ratio)][Q16n16_to_Q16n0(Q8n0_to_Q16n16(notes[i]) + (pitchbend << 3) * pitchbend_amp)]);
-           Serial.print(" ");
-           Serial.print(deviation_pot);
-           Serial.print(" ");
-           Serial.println(deviation_sm[i]);*/
-
       }
       break;
     case 3:
-      deviation_rm_pot = mozziAnalogRead(PA5) << 4 ;
+      deviation_rm_pot = mozziAnalogRead(PA5) << 4;
       //for (byte i = 0; i < POLYPHONY; i++)  compute_fm_param(i);
       break;
     case 4:
       breath_on_rm = (mozziAnalogRead(PA3) >> 4);
       break;
     case 5:
-      chord_release = mozziAnalogRead(PA7) >> 0 ;
+      chord_release = mozziAnalogRead(PA7) >> 0;
       break;
     case 6:
       chord_attack = mozziAnalogRead(PB1) >> 0;
@@ -343,7 +325,7 @@ Serial3.println("c");
       breath_on_cutoff = (mozziAnalogRead(PA4) >> 4);
       break;
     case 8:
-      resonance  = mozziAnalogRead(PA2);
+      resonance = mozziAnalogRead(PA2);
       break;
     case 9:
       breath_sens = mozziAnalogRead(PA1) >> 4;
@@ -351,7 +333,7 @@ Serial3.println("c");
       break;
   }
 
-  //for (byte i = 0; i < POLYPHONY; i++) deviation_sm[i] = deviation_smooth[i].next(deviation[i]);
+  //for (byte i = 0; i < POLYPHONY; i++) deviation_sm[i] = deviation_smooth[i].next(deviation[i]);/
   for (byte i = 0; i < POLYPHONY; i++) deviation_sm[i] = deviation[i];
   /*
     Serial.print(volume);
@@ -362,14 +344,13 @@ Serial3.println("c");
     Serial.println(tamp);*/
 }
 
+int countt = 0;
+
 AudioOutput_t updateAudio() {
-
   long sample = 0;
-
-  cutoff = cutoff_smooth.next(((breath_on_cutoff * volume) >> 6 ) + (midi_cutoff << 9)); // >>8
+  cutoff = cutoff_smooth.next(((breath_on_cutoff * volume) >> 6) + (midi_cutoff << 9));  // >>8
   if (cutoff > 65535) cutoff = 65535;
-  if (cutoff != prev_cutoff || resonance != prev_resonance)
-  {
+  if (cutoff != prev_cutoff || resonance != prev_resonance) {
     lpf.setCutoffFreqAndResonance(cutoff, resonance << 4);
     prev_cutoff = cutoff;
     prev_resonance = resonance;
@@ -380,7 +361,6 @@ AudioOutput_t updateAudio() {
   tamp_volume = tamp_volume >> 1;
   if (tamp_volume > 65535) tamp_volume = 65535;  //16 bits
   breath_next = breath_smooth.next(tamp_volume);
-
   //deviation_rm = rm_smooth.next(((breath_on_rm * volume)>>6) + deviation_rm_pot);
   deviation_rm = rm_smooth.next(((breath_on_rm * volume) >> 6) + deviation_rm_pot << 2);
   //if (deviation_rm > 524288) deviation_rm = 524288;
@@ -398,22 +378,19 @@ AudioOutput_t updateAudio() {
     }
   */
 
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
+  for (byte i = 0; i < POLYPHONY; i++) {
     envelope[i].update();
     int env_next = envelope[i].next();  // for enveloppe to roll even if it is not playing
     // if (!envelope[i].playing() && osc_is_on[i]) osc_is_on[i] = false;
-    if (envelope[i].playing() && osc_is_on[i])
-    {
+    if (envelope[i].playing() && osc_is_on[i]) {
       long partial_sample = 0;
+      Q15n16 modulation_rm = deviation_rm * aCarrier[i].next() >> 8;
+      Q15n16 modulation = deviation_sm[i] * aMod[i].phMod(modulation_rm) >> 8;
+      partial_sample = aCarrier[i].phMod(modulation);  // 8bits
 
-
-      Q15n16 modulation_rm = deviation_rm * aCarrier[i].next() >> 8 ;
-      Q15n16 modulation = deviation_sm[i] * aMod[i].phMod(modulation_rm) >> 8 ;
-      partial_sample = aCarrier[i].phMod(modulation); // 8bits
 
 #ifdef RINGING_CHORDS
-      if (breath_at_note_off[i] != 0 && breath_at_note_off[i] < breath_next)  partial_sample *= breath_at_note_off[i];
+      if (breath_at_note_off[i] != 0 && breath_at_note_off[i] < breath_next) partial_sample *= breath_at_note_off[i];
 
       else partial_sample *= breath_next;
 #endif
@@ -421,16 +398,14 @@ AudioOutput_t updateAudio() {
     }
   }
 #ifndef RINGING_CHORDS
-  sample = (sample * breath_next) ; //  //31
+  sample = (sample * breath_next);  //  //31
 #endif
 #ifdef DITHERING
   sample += rand(-16384, 16384);  //
 #endif
-
-  sample = lpf.next(sample >> 15); // that's 16bits, overflowing with chords?
+  sample = lpf.next(sample >> 15);  // that's 16bits, overflowing with chords?
 
 
 
   return MonoOutput::fromNBit(16, sample).clip();
-
 }
--- 10knobs_mono_16bits_PT8211/softwares/DIGITAL_SENAP_sax/midi_handles.ino
+++ 10knobs_mono_16bits_PT8211/softwares/DIGITAL_SENAP_sax/midi_handles.ino
@@ -6,14 +6,12 @@
 /*********************
        NOTE ON
 ********************/
-void HandleNoteOn(byte channel, byte note, byte velocity)
-{
+void HandleNoteOn(byte channel, byte note, byte velocity) {
   byte min_rank = 255;
   int empty_arg = -1;
   for (byte i = 0; i < POLYPHONY; i++)  //take a non playing oscil
   {
-    if (!envelope[i].playing() && oscil_rank[i] < min_rank)
-    {
+    if (!envelope[i].playing() && oscil_rank[i] < min_rank) {
       empty_arg = i;
       min_rank = oscil_rank[i];
     }
@@ -22,10 +20,8 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
   if (empty_arg == -1)  //kill a oscil in release phase
   {
     min_rank = 255;
-    for (byte i = 0; i < POLYPHONY; i++)
-    {
-      if (oscil_state[i] == 0 && oscil_rank[i] < min_rank)
-      {
+    for (byte i = 0; i < POLYPHONY; i++) {
+      if (oscil_state[i] == 0 && oscil_rank[i] < min_rank) {
         empty_arg = i;
         min_rank = oscil_rank[i];
       }
@@ -37,12 +33,10 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
   {
     min_rank = 255;
     byte min_rank_arg = 0;
-    for (byte i = 0; i < POLYPHONY; i++)
-    {
-      if (oscil_rank[i] < min_rank)
-      {
+    for (byte i = 0; i < POLYPHONY; i++) {
+      if (oscil_rank[i] < min_rank) {
         min_rank = oscil_rank[i];
-        min_rank_arg  = i;
+        min_rank_arg = i;
       }
     }
     empty_arg = min_rank_arg;
@@ -53,18 +47,15 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
   set_freq(empty_arg, true);
   //aMod[empty_arg].setPhaseFractional(aCarrier[empty_arg].getPhaseFractional());
 
-  if (channel == 2)
-  {
+  if (channel == 2) {
     envelope[empty_arg].setAttackTime(1);
     envelope[empty_arg].noteOn();
   }
 
-  else if (channel == 3 && chord_attack > 5 )
-  {
+  else if (channel == 3 && chord_attack > 5) {
     envelope[empty_arg].setAttackTime(chord_attack);
     envelope[empty_arg].noteOn(true);
-  }
-  else  // channel==3 and attach <=5
+  } else  // channel==3 and attach <=5
   {
     envelope[empty_arg].setAttackTime(1);
     envelope[empty_arg].noteOn(true);
@@ -77,8 +68,7 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
 
 
   byte max_rank = 0;
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
+  for (byte i = 0; i < POLYPHONY; i++) {
     if (oscil_rank[i] > max_rank) max_rank = oscil_rank[i];
   }
   oscil_rank[empty_arg] = max_rank + 1;
@@ -86,20 +76,18 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
 
   // shift all oscill
   min_rank = 255;
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
-    if (oscil_rank[i] < min_rank)
-    {
+  for (byte i = 0; i < POLYPHONY; i++) {
+    if (oscil_rank[i] < min_rank) {
       empty_arg = i;
       min_rank = oscil_rank[i];
     }
   }
-  if (min_rank != 0) for (byte i = 0; i < POLYPHONY; i++) oscil_rank[i] -= min_rank;
+  if (min_rank != 0)
+    for (byte i = 0; i < POLYPHONY; i++) oscil_rank[i] -= min_rank;
 
   //volume =(int) velocity << 7 ;
   volume &= 0b00000001111111;
-  volume += (int) velocity << 7;
-
+  volume += (int)velocity << 7;
 }
 
 
@@ -108,23 +96,19 @@ void HandleNoteOn(byte channel, byte note, byte velocity)
 *********************/
 
 
-void HandleNoteOff(byte channel, byte note, byte velocity)
-{
+void HandleNoteOff(byte channel, byte note, byte velocity) {
   byte to_kill = 255;
   byte min_rank = 255;
 
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
-    if (note == notes[i] && oscil_state[i] == 1 && oscil_rank[i] < min_rank  )
-    {
+  for (byte i = 0; i < POLYPHONY; i++) {
+    if (note == notes[i] && oscil_state[i] == 1 && oscil_rank[i] < min_rank) {
       min_rank = oscil_rank[i];
       to_kill = i;
     }
   }
 
-  if (to_kill != 255)
-  {
-    if (channel == 2)  envelope[to_kill].setReleaseTime(1);
+  if (to_kill != 255) {
+    if (channel == 2) envelope[to_kill].setReleaseTime(1);
     else if (channel == 3) {
       envelope[to_kill].setReleaseTime(chord_release);
 #ifdef RINGING_CHORDS
@@ -135,8 +119,7 @@ void HandleNoteOff(byte channel, byte note, byte velocity)
     oscil_state[to_kill] = 0;
   }
 
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
+  for (byte i = 0; i < POLYPHONY; i++) {
     if (oscil_rank[i] > oscil_rank[to_kill]) oscil_rank[i] -= 1;
   }
 }
@@ -146,63 +129,54 @@ void HandleNoteOff(byte channel, byte note, byte velocity)
 /*************************
   CONTROL CHANGE
 ************************/
-void HandleControlChange(byte channel, byte control, byte val)
-{
-  switch (control)
-  {
-    case 64:    // sustain
-      if (val == 0)
-      {
+void HandleControlChange(byte channel, byte control, byte val) {
+  switch (control) {
+    case 64:  // sustain
+      if (val == 0) {
         sustain = false;
-        for (byte i = 0; i < POLYPHONY; i++)
-        {
-          if (oscil_state[i] == 2)
-          {
+        for (byte i = 0; i < POLYPHONY; i++) {
+          if (oscil_state[i] == 2) {
             envelope[i].noteOff();
             oscil_state[i] = 0;
           }
         }
-      }
-      else sustain = true;
+      } else sustain = true;
       break;
 
 
-    /*  case 1:  //modulation
+      /*  case 1:  //modulation
       for (byte i = 0; i < POLYPHONY; i++) LFO[i].setFreq_Q16n16((Q16n16) (val << 13 ));
       if (val == 0 && mod) mod = false;
       else if (val != 0 && !mod) mod = true;
       break;*/
 
-    case 74: //volume (MSB)
-      //if (val > prev_MSB_volume || (val & 0b00000001111111) != 0) volume = (int) (val) << 7;
-        if (val > prev_MSB_volume) volume = (int) (val) << 7;
-	else if (val == 0)
-	  {
-	    volume &= 0b00000001111111;
-            volume += val << 7;
-	  }
-        else volume = ((int) (val) << 7) + 0b00000001111111;
-        prev_MSB_volume = val;
-	//volume &= 0b00000001111111;
-	//volume += val << 7;
-	break;
+    case 74:  //volume (MSB)
+              //if (val > prev_MSB_volume || (val & 0b00000001111111) != 0) volume = (int) (val) << 7;
+      if (val > prev_MSB_volume) volume = (int)(val) << 7;
+      else if (val == 0) {
+        volume &= 0b00000001111111;
+        volume += val << 7;
+      } else volume = ((int)(val) << 7) + 0b00000001111111;
+      prev_MSB_volume = val;
+      //volume &= 0b00000001111111;
+      //volume += val << 7;
+      break;
 
 
 
-    case 75: //volume LSB
+    case 75:  //volume LSB
       volume &= 0b11111110000000;
       volume += val;
 
       break;
 
-    case 71: //cutoff
+    case 71:  //cutoff
       midi_cutoff = val;
       break;
 
-    case 5: // pitchbend_amp
+    case 5:  // pitchbend_amp
       pitchbend_amp = val;
       break;
-
   }
 }
 
@@ -211,11 +185,9 @@ void HandleControlChange(byte channel, byte control, byte val)
 /**********************
       PITCHBEND
       *    ***************/
-void HandlePitchBend(byte channel, int bend)
-{
+void HandlePitchBend(byte channel, int bend) {
   pitchbend = bend;
-  for (byte i = 0; i < POLYPHONY; i++)
-  {
+  for (byte i = 0; i < POLYPHONY; i++) {
     if (envelope[i].playing() && osc_is_on[i]) set_freq(i, false);
   }
 }
